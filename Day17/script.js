42822b2ae4fc2a707deea2c783db803ae79207d3
Domitius <domitius.rc@gmail.com>
2021-09-09 22:09:40 -0500

    Day17

---
+++ Day17/script.js
@@ -0,0 +1,64 @@
+const API_KEY = '**API_KEY**';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=' + API_KEY + '&page=1';
+const IMAGE_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=' + API_KEY + '&query="';
+
+// DOM Elements
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+getMovies(API_URL);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm.length > 2) {
+        getMovies(SEARCH_API + searchTerm);
+        search.value = '';
+    } else {
+        window.location.reload();
+    }
+});
+
+async function getMovies(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+            <img src="${IMAGE_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
